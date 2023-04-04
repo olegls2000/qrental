b52c738cf42de9fd51983375de55c62a63f278ff
Oleg Pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2023-04-04 22:34:31 +0300

    Added 'withVat' property to Transaction

--- source/ui-thymeleaf/src/main/java/ee/qrental/ui/controller/ControllerConfig.java
+++ source/ui-thymeleaf/src/main/java/ee/qrental/ui/controller/ControllerConfig.java
@@ -1,6 +1,6 @@
 package ee.qrental.ui.controller;
 
-dimport org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
