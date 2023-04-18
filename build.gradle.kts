b63e522917b7c3e71d17280691d4facc7616c0d9
Oleg Pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2023-04-18 09:48:06 +0300

    Fixed Java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,22 @@
 plugins {
     id("io.spring.dependency-management") version "1.1.0"
+    id("java")
+    id("java-library")
 }
 group = "ee.qrental"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 subprojects {
     apply {
         plugin("java")
         plugin("java-library")
         plugin("io.spring.dependency-management")
     }
+
 
     dependencyManagement {
         imports {
