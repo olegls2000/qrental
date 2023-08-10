c9f6a72cab0d5fbc35a52c37ecd1a2fd33cd3e43
Oleg Pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2023-08-10 22:16:32 +0300

    - time formatting for cars table view.

--- source/ui-thymeleaf/src/main/java/ee/qrental/ui/controller/car/CarQueryController.java
+++ source/ui-thymeleaf/src/main/java/ee/qrental/ui/controller/car/CarQueryController.java
@@ -1,24 +1,27 @@
 package ee.qrental.ui.controller.car;
 
 import ee.qrental.car.api.in.query.GetCarQuery;
+import ee.qrental.ui.controller.formatter.QDateFormatter;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static ee.qrental.ui.controller.formatter.QDateFormatter.MODEL_ATTRIBUTE_DATE_FORMATTER;
 import static ee.qrental.ui.controller.util.ControllerUtils.CAR_ROOT_PATH;
 
 @Controller
 @RequestMapping(CAR_ROOT_PATH)
 @AllArgsConstructor
 public class CarQueryController {
-
+  private final QDateFormatter qDateFormatter;
   private final GetCarQuery carQuery;
 
   @GetMapping
   public String getCarView(final Model model) {
     model.addAttribute("cars", carQuery.getAll());
+    model.addAttribute(MODEL_ATTRIBUTE_DATE_FORMATTER, qDateFormatter);
 
     return "cars";
   }
