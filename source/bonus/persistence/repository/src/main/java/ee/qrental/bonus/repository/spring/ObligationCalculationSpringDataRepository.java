c5e125fff26851dc786dc0b479c03862bd1948cf
Oleg Pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2024-01-22 09:05:16 +0200

    Bug fix. Ordering of results didn't count to order of week numbers

--- source/bonus/persistence/repository/src/main/java/ee/qrental/bonus/repository/spring/ObligationCalculationSpringDataRepository.java
+++ source/bonus/persistence/repository/src/main/java/ee/qrental/bonus/repository/spring/ObligationCalculationSpringDataRepository.java
@@ -12,7 +12,7 @@ public interface ObligationCalculationSpringDataRepository
                     "select qw.id "
                             + "from obligation_calculation oc "
                             + "         LEFT JOIN q_week qw on oc.q_week_id = qw.id "
-                            + "order by qw.year, qw.number desc "
+                            + "order by qw.year desc, qw.number desc "
                             + "limit 1;",
             nativeQuery = true)
     Long getLastCalculationQWeekId();
