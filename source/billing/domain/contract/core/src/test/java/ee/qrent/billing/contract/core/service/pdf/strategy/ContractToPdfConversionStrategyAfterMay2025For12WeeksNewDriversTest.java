f7338a5c9d76f822870ce665a9dc4e69da4cf0b7
Oleg Pasichnyk <pasechnik.o.n@gmail.com>
2025-04-29 14:54:21 +0300

    - Added ContractToPdfConversionStrategyAfterMay2025For12WeeksNewDriversTest

---
+++ source/billing/domain/contract/core/src/test/java/ee/qrent/billing/contract/core/service/pdf/strategy/ContractToPdfConversionStrategyAfterMay2025For12WeeksNewDriversTest.java
@@ -0,0 +1,86 @@
+package ee.qrent.billing.contract.core.service.pdf.strategy;
+
+import ee.qrent.billing.contract.api.out.ContractLoadPort;
+import ee.qrent.billing.contract.core.service.pdf.ContractPdfModel;
+import ee.qrent.billing.contract.domain.Contract;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static java.util.Collections.singletonList;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ContractToPdfConversionStrategyAfterMay2025For12WeeksNewDriversTest {
+  private ContractToPdfConversionStrategyAfterMay2025For12WeeksNewDrivers instanceUnderTest;
+  private ContractLoadPort loadPort;
+
+  @BeforeEach
+  void init() {
+    loadPort = mock(ContractLoadPort.class);
+    instanceUnderTest =
+        new ContractToPdfConversionStrategyAfterMay2025For12WeeksNewDrivers(loadPort);
+  }
+
+  @Test
+  public void testIfDriverIsNewTwelveWeeksStartedBeforeMay2025() {
+    // given
+    final var driverId = 11L;
+    final var contract = Contract.builder().build();
+    final var pdfModel =
+        ContractPdfModel.builder()
+            .driverId(driverId)
+            .dateStart(LocalDate.of(2025, Month.APRIL, 27))
+            .duration("kaksteist")
+            .build();
+    when(loadPort.loadAllByDriverId(driverId)).thenReturn(singletonList(contract));
+
+    // when
+    final var canApply = instanceUnderTest.canApply(pdfModel);
+
+    // then
+    assertFalse(canApply);
+  }
+  @Test
+  public void testIfDriverIsNewFourWeeksStartedAfterMay2025() {
+    // given
+    final var driverId = 11L;
+    final var contract = Contract.builder().build();
+    final var pdfModel =
+            ContractPdfModel.builder()
+                    .driverId(driverId)
+                    .dateStart(LocalDate.of(2025, Month.APRIL, 28))
+                    .duration("neli")
+                    .build();
+    when(loadPort.loadAllByDriverId(driverId)).thenReturn(singletonList(contract));
+
+    // when
+    final var canApply = instanceUnderTest.canApply(pdfModel);
+
+    // then
+    assertFalse(canApply);
+  }
+
+  @Test
+  public void testIfDriverIsNewTwelveWeeksStartedAfterMay2025() {
+    // given
+    final var driverId = 11L;
+    final var contract = Contract.builder().build();
+    final var pdfModel =
+            ContractPdfModel.builder()
+                    .driverId(driverId)
+                    .dateStart(LocalDate.of(2025, Month.APRIL, 28))
+                    .duration("kaksteist")
+                    .build();
+    when(loadPort.loadAllByDriverId(driverId)).thenReturn(singletonList(contract));
+
+    // when
+    final var canApply = instanceUnderTest.canApply(pdfModel);
+
+    // then
+    assertTrue(canApply);
+  }
+}
