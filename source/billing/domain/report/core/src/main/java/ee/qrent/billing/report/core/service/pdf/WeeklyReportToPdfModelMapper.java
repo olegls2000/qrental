bf28e67575b36e266f73db85110ff8f99f1e3686
Oleg Pasichnyk <pasechnik.o.n@gmail.com>
2025-08-19 17:47:39 +0300

    - Extension: added Amounts map to the weekly reports

--- source/billing/domain/report/core/src/main/java/ee/qrent/billing/report/core/service/pdf/WeeklyReportToPdfModelMapper.java
+++ source/billing/domain/report/core/src/main/java/ee/qrent/billing/report/core/service/pdf/WeeklyReportToPdfModelMapper.java
@@ -10,40 +10,44 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class WeeklyReportToPdfModelMapper {
 
-    private final GetQWeekQuery qWeekQuery;
-    private final GetDriverQuery driverQuery;
-    private final GetCallSignQuery callSignQuery;
-    private final GetCarQuery carQuery;
+  private final GetQWeekQuery qWeekQuery;
+  private final GetDriverQuery driverQuery;
+  private final GetCallSignQuery callSignQuery;
+  private final GetCarQuery carQuery;
 
-    public WeeklyReportPdfModel getPdfModel(final WeeklyReport report) {
-        final var driver = driverQuery.getById(report.getDriverId());
-        final var callSign = callSignQuery.getById(report.getCallSignId());
-        final var previousWeek = qWeekQuery.getById(report.getQWeekId());
-        final var currentWeek = qWeekQuery.getOneAfterById(report.getQWeekId());
-        final var carReg = report.getCarId() != null ? carQuery.getById(report.getCarId()).getRegNumber() : null;
+  public WeeklyReportPdfModel getPdfModel(final WeeklyReport report) {
+    final var driver = driverQuery.getById(report.getDriverId());
+    final var callSign = callSignQuery.getById(report.getCallSignId());
+    final var previousWeek = qWeekQuery.getById(report.getQWeekId());
+    final var currentWeek = qWeekQuery.getOneAfterById(report.getQWeekId());
+    final var carReg =
+        report.getCarId() != null ? carQuery.getById(report.getCarId()).getRegNumber() : null;
 
-        return WeeklyReportPdfModel.builder()
-                .firstName(driver.getFirstName())
-                .lastName(driver.getLastName())
-                .taxNumber(driver.getTaxNumber())
-                .callSign(callSign.getCallSign())
-                .previousWeekStart(previousWeek.getStart())
-                .previousWeekEnd(previousWeek.getEnd())
-                .currentWeekStart(currentWeek.getStart())
-                .currentWeekEnd(currentWeek.getEnd())
-                .feeAmountSunday(report.getFeeAmountSunday())
-                .carRegistrationNumber(carReg)
-                .depositObligation(report.getDepositObligation())
-                .depositPaid(report.getDepositPaid())
-                .balanceAmountSunday(report.getBalanceAmountSunday())
-                .balanceAmountAtCalculationMoment(report.getBalanceAmountAtCalculationMoment())
-                .weeksCountTillEnd(report.getWeeksCountTillEnd())
-                .obligationStatus(report.getObligationStatus() != null ? report.getObligationStatus().name() : null)
-                .comment(report.getComment())
-                // amount: prefer balance at calculation moment, fallback to balance on Sunday
-                .amount(report.getBalanceAmountAtCalculationMoment() != null
-                        ? report.getBalanceAmountAtCalculationMoment()
-                        : report.getBalanceAmountSunday())
-                .build();
-    }
+    return WeeklyReportPdfModel.builder()
+        .firstName(driver.getFirstName())
+        .lastName(driver.getLastName())
+        .taxNumber(driver.getTaxNumber())
+        .callSign(callSign.getCallSign())
+        .previousWeekStart(previousWeek.getStart())
+        .previousWeekEnd(previousWeek.getEnd())
+        .currentWeekStart(currentWeek.getStart())
+        .currentWeekEnd(currentWeek.getEnd())
+        .feeAmountSunday(report.getFeeAmountSunday())
+        .carRegistrationNumber(carReg)
+        .depositObligation(report.getDepositObligation())
+        .depositPaid(report.getDepositPaid())
+        .balanceAmountSunday(report.getBalanceAmountSunday())
+        .balanceAmountAtCalculationMoment(report.getBalanceAmountAtCalculationMoment())
+        .weeksCountTillEnd(report.getWeeksCountTillEnd())
+        .obligationStatus(
+            report.getObligationStatus() != null ? report.getObligationStatus().name() : null)
+        .transactionTypesVsAmount(report.getTransactionTypesVsAmount())
+        .comment(report.getComment())
+        // amount: prefer balance at calculation moment, fallback to balance on Sunday
+        .amount(
+            report.getBalanceAmountAtCalculationMoment() != null
+                ? report.getBalanceAmountAtCalculationMoment()
+                : report.getBalanceAmountSunday())
+        .build();
+  }
 }
