ba22987a15ee755896417f3c3c8cd9e10c4b2ef4
Oleg pasichnyk <pasechnik.o.n@gmail.com>
2025-10-07 22:52:15 +0300

    Added Weekly Info report

--- source/billing/domain/report/persistence/entity/src/main/java/ee/qrent/billing/report/persistence/entity/jakarta/WeeklyReportTypeJakarta.java
+++ source/billing/domain/report/persistence/entity/src/main/java/ee/qrent/billing/report/persistence/entity/jakarta/WeeklyReportTypeJakarta.java
@@ -1,7 +1,7 @@
 package ee.qrent.billing.report.persistence.entity.jakarta;
 
 public enum WeeklyReportTypeJakarta {
-  INFO,
+  INFO_REPORT,
   MONDAY_REPORT,
   TUESDAY_REPORT,
   WEDNESDAY_REPORT,
