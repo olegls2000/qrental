edc7a1c0a92a7ff3e303b786196b07a34f979e26
Oleg pasichnyk <pasechnik.o.n@gmail.com>
2025-10-15 23:57:18 +0300

    Added update script

--- source/billing/domain/report/report-helper.sql
+++ source/billing/domain/report/report-helper.sql
@@ -8,3 +8,6 @@ delete
 from weekly_report;
 
 --update driver set  need_report  = false;
+
+update weekly_report set type = 'TUESDAY_REPORT' where type = 'MONDAY_REPORT';
+update weekly_report_calculation  set type = 'TUESDAY_REPORT' where type = 'MONDAY_REPORT';
