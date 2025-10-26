e7c109355af19552087f046b7e104fb4c76da51c
Oleg pasichnyk <pasechnik.o.n@gmail.com>
2025-10-26 13:48:50 +0200

    Fixed problem related to deletion of transaction already participated in report calculation. Deletion became allowed.

--- source/billing/domain/report/persistence/flyway/src/main/resources/db/migration/billing/V20251026133410__add_delete_cascade_for_the_report_relted_tables_for_transactions_fk.sql
+++ source/billing/domain/report/persistence/flyway/src/main/resources/db/migration/billing/V20251026133410__add_delete_cascade_for_the_report_relted_tables_for_transactions_fk.sql
@@ -1,8 +1,8 @@
 ALTER TABLE weekly_report_transaction
-    DROP CONSTRAINT transaction_id_fkey;
+    DROP CONSTRAINT  weekly_report_transaction_transaction_id_fkey;
 
 ALTER TABLE weekly_report_transaction
-    ADD CONSTRAINT transaction_id_fkey_delete_cascade
+    ADD CONSTRAINT  weekly_report_transaction_transaction_id_fkey_delete_cascade
         FOREIGN KEY (transaction_id)
             REFERENCES transaction (id)
             ON DELETE CASCADE;
